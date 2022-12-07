bf350b532c7c84d4ec4645b6d0615fa98648cede
blgsn <111683241+blgsn@users.noreply.github.com>
2022-12-07 12:12:34 +0300

    Update CartPage.java

--- TestSel/src/main/java/pages/CartPage.java
+++ TestSel/src/main/java/pages/CartPage.java
@@ -7,17 +7,17 @@ import java.util.List;
 
 public class CartPage extends BasePage {
 
-    By productNameLocator = new By.ByCssSelector("a.cx-link");
-    By endShippingButton = By.cssSelector("#btnGoToShippingAddress > .ng-star-inserted");
+    By productNameLocator = new By.ByCssSelector("a.cx-link"); 
+    By endShippingButton = By.cssSelector("#btnGoToShippingAddress > .ng-star-inserted");   //complete purchase button
   	
     public CartPage(WebDriver driver) {
         super(driver);
     }
-
+    //The part that checks if the product has been added to the cart
     public boolean checkIfProductAdded() {
-        return getProducts().size() > 0 ;
+        return getProducts().size() > 0 ;  
     }
-
+    //access all products added to cart
     private List<WebElement> getProducts(){
         return findAll(productNameLocator);
     }
@@ -26,7 +26,7 @@ public class CartPage extends BasePage {
 		public void endShopping() {
 			driver.findElement(endShippingButton).click();
 		}
-		
+		//allows us to check if we are on the right page
 		public boolean isCorrectCartPage() {
 	        return isDisplayed(endShippingButton);
 	    }
