269edf64b9831460b9ea53f23c854d0b654d8cae
blgsn <111683241+blgsn@users.noreply.github.com>
2022-12-07 12:20:41 +0300

    Update HomePage.java

--- TestSel/src/main/java/pages/HomePage.java
+++ TestSel/src/main/java/pages/HomePage.java
@@ -7,7 +7,7 @@ public class HomePage extends BasePage {
 
 	SearchBox searchBox ;
     By cartCountLocator = new By.ByCssSelector("#iconMiniCart > .number");
-    By acceptCookiesLocator = By.id("ins-close-button-c2703");
+    By acceptCookiesLocator = By.id("ins-close-button-c2703"); 
     
     By loginLocator = new By.ByCssSelector("ul[class=nav nav-pills]");
     
@@ -16,7 +16,7 @@ public class HomePage extends BasePage {
         super(driver);
         searchBox = new SearchBox(driver);
     }
-    public void acceptCookies(){
+    public void acceptCookies(){    //If there is a pop up on the page, it will turn it off
         if (isDisplayed(acceptCookiesLocator)){
         	click(acceptCookiesLocator);
         }
@@ -25,16 +25,16 @@ public class HomePage extends BasePage {
     public SearchBox searchBox(){
         return this.searchBox;
     }
-
-    public boolean isProductCountUp() {
+    //Checks if the product has been added to the cart, if the number in the cart icon has increased, it has been added
+    public boolean isProductCountUp() {  
         return getCartCount() > 0 ;
     }
-
+    //Converts the number in the basket symbol from string to integer
     private int getCartCount(){
         String count = find(cartCountLocator).getText();
         return Integer.parseInt(count);
     }
-    
+    //Checking that we are logged in after logging in to the page throws it to the homepage after logging in
     public boolean isCorrectLogin() {
         return isDisplayed(loginLocator);
     }
