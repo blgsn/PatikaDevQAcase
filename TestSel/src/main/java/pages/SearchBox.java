9eb3f6103aa047a965dc2bc862959d09c53bb1a1
blgsn <111683241+blgsn@users.noreply.github.com>
2022-12-07 12:29:36 +0300

    Update SearchBox.java

--- TestSel/src/main/java/pages/SearchBox.java
+++ TestSel/src/main/java/pages/SearchBox.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.WebDriver;
 
 public class SearchBox extends BasePage{
 	
-    By searchBoxLocator = By.id("txtSearchBox");
+    By searchBoxLocator = By.id("txtSearchBox");  //search box 
     
     public SearchBox(WebDriver driver) {
         super(driver);
     }
-   
+   //method of send text to searchbox
     public void search(String text) {
         type(searchBoxLocator,text);
        
